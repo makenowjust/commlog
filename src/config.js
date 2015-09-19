d60e5f07040e1a4da562c18e4830e4a99af82d1b
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-09-19 22:39:07 +0900

    Update oauth token

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_ENTRY_POINT = 'https://api.github.com';
 export const REPO_NAME = 'MakeNowJust/commlog';
 export const REPO_BRANCH = 'commlog';
-export const OAUTH_TOKEN = 'f409b8288b08b4f07f4c329fdffdf59abb674211';
+export const OAUTH_TOKEN = '28ff173c802e57b8d7d0aaf8fe55dba34d731ca3';

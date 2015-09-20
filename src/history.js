92f54dc4a63b53af2c3b3a12d01f8b6911e40a83
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-09-20 22:43:54 +0900

    Disabled appending `_k=xxx` into URL

---
+++ src/history.js
@@ -0,0 +1,5 @@
+import { createHashHistory } from 'history';
+
+export default createHashHistory({
+  queryKey: false, // to be disable appending `?_k=xxx` into URL
+});
--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ import {
   Route, IndexRoute
 } from 'react-router';
 import { Provider } from 'react-redux';
-import store from './store';
 import App from './components/app';
 import CommitListPage from './containers/commit-list-page';
 import CommitPage from './containers/commit-page';
+import store from './store';
+import history from './history';
 
 React.render(
   <Provider store={store}>
     {() => (
-      <Router>
+      <Router history={history}>
         <Route path='/' component={App}>
           <IndexRoute component={CommitListPage} />
           <Route path='commit/:sha' component={CommitPage} />

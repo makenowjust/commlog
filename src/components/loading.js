7c1be73b2f853f94d30892f2e2594cea1f20de07
TSUYUSATO Kitsune <make.just.on@gmail.com>
2015-09-24 19:03:16 +0900

    Add loading component

---
+++ src/components/loading.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Radium from 'radium';
+
+@Radium
+export default class Loading extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div style={[style.loading]}>
+        <div>
+          {this.props.children}
+        </div>
+        <img src='img/box.gif' style={[style.box]} />
+      </div>
+    );
+  }
+}
+
+const style = {
+  loading: {
+    padding: '1em 0',
+    textAlign: 'center',
+    width: '100%',
+    height: '14em',
+  },
+  box: {
+    paddingTop: '1em',
+    width: '10em',
+    height: '10em',
+  },
+};

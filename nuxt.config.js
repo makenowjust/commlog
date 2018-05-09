7022b661c53ae2aac4b77e5d0f65c2343d312762
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-05-09 22:28:41 +0900

    feat: add index.vue skeleton

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,5 +2,14 @@ export default {
   mode: 'spa',
   css: [
     'modern-normalize',
+    '@/assets/css/main.css',
   ],
+  head: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Raleway',
+      },
+    ],
+  },
 };
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <header :class="$style.header">
+      <h1>commlog</h1>
+    </header>
+  </div>
+</template>
+
+<style module>
+.header {
+  text-align: center;
+  margin: 2em 0;
+}
+</style>

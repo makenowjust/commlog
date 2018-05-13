5231a1958760d375851a05f080e9c03932e09d33
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-05-13 17:08:31 +0900

    feat: add meta in head

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
     '@/assets/scss/main.scss',
   ],
   head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
     link: [
       {
         rel: 'stylesheet',

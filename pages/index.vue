bcb882abd2879a91fb10974a8ec95d5edce280f3
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-05-13 14:35:57 +0900

    feat: load commits

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,107 @@
 <template>
   <div>
-    <commit hash="832cde60d7f0c114a4c9fe0a746d2105e5665a52" />
+    <commit v-for="hash in hashes" :key="hash" :hash="hash" />
+
+    <template v-if="next">
+      <section v-if="loading" :class="$style.loading">
+        <pre v-if="loadError">{{loadError.stack}}</pre>
+        <p v-else>Loading...</p>
+      </section>
+      <section v-else :class="$style.loadMore" @click="loadMore">
+        <p>Load more...</p>
+      </section>
+    </template>
   </div>
 </template>
 
+<style module lang="scss">
+@import "~@/assets/scss/variables";
+
+.loading, .loadMore {
+  max-width: $max-width;
+  padding: 5rem 0;
+  margin: 0 auto;
+
+  > p {
+    text-align: center;
+  }
+}
+
+.loadMore {
+  cursor: pointer;
+  color: $black;
+  transition: color ease 0.3s 0s;
+
+  &:hover {
+    color: $light-blue;
+  }
+}
+</style>
+
 <script>
+import * as parseLinkHeader from 'parse-link-header';
+
 import Commit from '~/components/Commit.vue';
+
+const fetchCommits = async (
+  {$axios, $store},
+  url = 'https://api.github.com/repos/MakeNowJust/commlog/commits?sha=commlog&page=1',
+) => {
+  const result = await $axios.get(url);
+
+  const rawCommits = result.data;
+  const commits = rawCommits.map(raw => ({
+    hash: raw.sha,
+    message: raw.commit.message,
+    author: {
+      name: raw.author && raw.author.login || raw.commit.author.name,
+      email: raw.commit.author.email,
+      icon: raw.author && raw.author.avatar_url,
+    },
+    date: new Date(raw.commit.author.date),
+  }));
+
+  for (const commit of commits) {
+    $store.commit('commits/put', {commit});
+  }
+
+  const link = parseLinkHeader(result.headers.link);
+
+  return {
+    commits,
+    next: link.next && link.next.url,
+  };
+};
 
 export default {
   components: {Commit},
-  fetch({store}) {
-    const commit = {
-      hash: '832cde60d7f0c114a4c9fe0a746d2105e5665a52',
-      author: {
-        name: 'MakeNowJust',
-        email: 'make.just.on@gmail.com',
-        icon: 'https://avatars3.githubusercontent.com/u/6679325?v=4',
-      },
-      date: new Date('2018-05-12T01:53:56Z'),
-      message: `
-# bat
-
-https://github.com/sharkdp/bat
-
-\`cat\`のクローンと言っているが少し違う気がする。
-
-シンタックスハイライトしてくれたり、自動で出力をlessに流してくれたりする感じ。
-
-Rustで書かれてる。
-`,
+  data() {
+    return {
+      loading: false,
+      loadError: null,
     };
-    store.commit('commits/put', {commit});
-  }
+  },
+  async asyncData({$axios, store: $store}) {
+    const {commits, next} = await fetchCommits({$axios, $store});
+
+    return {
+      next,
+      hashes: commits.map(({hash}) => hash),
+    };
+  },
+  methods: {
+    async loadMore() {
+      try {
+        this.loading = true;
+        const {commits, next} = await fetchCommits(this, this.next);
+        this.hashes = this.hashes.concat(commits.map(({hash}) => hash));
+        this.next = next;
+      } catch (err) {
+        this.loadError = err;
+      } finally {
+        this.loading = false;
+      }
+    }
+  },
 };
 </script>

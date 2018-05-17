37ef4fbdbe31a06c67001834ee0e9ffee5768299
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-05-17 22:21:28 +0900

    feat: add error page layout

---
+++ layouts/error.vue
@@ -0,0 +1,31 @@
+<template>
+  <section :class="$style.error">
+    <h2>{{error.message}}</h2>
+    <details v-if="error.stack">
+      <summary>Stack trace</summary>
+      <pre>{{error.stack}}</pre>
+    </details>
+  </section>
+</template>
+
+<style module lang="scss">
+@import "~@/assets/scss/variables";
+
+.error {
+  max-width: $max-width;
+  padding: 5rem 0;
+  margin: 0 auto;
+  text-align: center;
+  overflow: auto;
+
+  details, pre {
+    text-align: left;
+  }
+}
+</style>
+
+<script>
+export default {
+  props: ['error']
+};
+</script>

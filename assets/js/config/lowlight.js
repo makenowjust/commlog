03f0634b9dc416ab6cb00a9c6029857defb5a5bc
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-05-19 13:04:52 +0900

    fix: add languages to highlight

--- assets/js/config/lowlight.js
+++ assets/js/config/lowlight.js
@@ -3,13 +3,17 @@ export * from 'lowlight/lib/core';
 import * as low from 'lowlight/lib/core';
 
 import * as bash from 'highlight.js/lib/languages/shell';
+import * as css from 'highlight.js/lib/languages/css';
 import * as crystal from 'highlight.js/lib/languages/crystal';
 import * as javascript from 'highlight.js/lib/languages/javascript';
 import * as ruby from 'highlight.js/lib/languages/ruby';
 import * as shell from 'highlight.js/lib/languages/shell';
+import * as xml from 'highlight.js/lib/languages/xml';
 
 low.registerLanguage('bash', bash);
+low.registerLanguage('css', css);
 low.registerLanguage('crystal', crystal);
-low.registerLanguage('jabascript', javascript);
+low.registerLanguage('javascript', javascript);
 low.registerLanguage('ruby', ruby);
 low.registerLanguage('shell', shell);
+low.registerLanguage('xml', xml);

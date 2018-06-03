f1b361d3441279737eba1e9adf0ff6d0ce0d9973
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-06-04 05:46:20 +0900

    refactor: separate markdown converting from component

--- components/CommitBody.vue
+++ components/CommitBody.vue
@@ -11,35 +11,13 @@
 </style>
 
 <script>
-import * as markdown from 'remark-parse';
-import * as rehype2react from 'rehype-react';
-import * as remark2rehype from 'remark-rehype';
-import * as highlight from 'rehype-highlight';
-import * as unified from 'unified';
+import markdown from '~/lib/markdown';
 
 export default {
   props: ['source'],
   render(h) {
-    const createElement = (tagName, props, children) => {
-      const {className, ...attrs} = props || {};
-      return h(
-        tagName,
-        {
-          class: className,
-          attrs,
-        },
-        children,
-      );
-    };
-
-    const processor = unified()
-      .use(markdown)
-      .use(remark2rehype)
-      .use(highlight, {ignoreMissing: true, subset: false})
-      .use(rehype2react, {createElement});
-    const {contents} = processor.processSync(this.source);
-
-    return createElement('section', {class: this.$style.wrap}, [contents]);
+    const contents = markdown(h, this.source);
+    return h('section', {class: this.$style.wrap}, [contents]);
   },
 };
 </script>

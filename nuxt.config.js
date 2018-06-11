443683f81ef6b9cdce3562b56d50ab9539d8fe1f
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-06-11 21:27:46 +0900

    feat: add sentry module

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       },
     ],
   },
-  modules: ['@nuxtjs/axios'],
+  modules: ['@nuxtjs/axios', '@nuxtjs/sentry'],
   plugins: ['~/plugins/axios', '~/plugins/test-directive'],
   router: {
     base: '/commlog/',
@@ -34,4 +34,7 @@ export default {
       config.resolve.alias['lowlight$'] = '~/lib/lowlight';
     },
   },
+  sentry: {
+    dsn: 'https://cd350f3bc93247049a7c1d3bfea4ccbc@sentry.io/1207111',
+  }
 };

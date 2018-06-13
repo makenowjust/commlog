52ddf918796345e78233c1d5042383ab9de04f37
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-06-13 22:50:03 +0900

    fix: simplify commit HTML

--- components/Commit.vue
+++ components/Commit.vue
@@ -1,14 +1,16 @@
 <template>
   <article :class="$style.wrap" v-test="`commit commit-${hash.slice(0, 7)}`">
     <commit-body :tree="tree" />
-    <section :class="$style.foot"><!--
-      --><a v-if="author.github" :class="$style.author" :href="`https://github.com/${author.name}`"><!--
-        -->{{author.name}}<img :src="author.icon" /><!--
-      --></a><!--
-      --><span v-else :class="$style.author">{{author.name}}</span><!--
-      --><time :datetime="date.toISOString()">{{date.toLocaleString()}}</time><!--
-      --><nuxt-link :class="$style.hash" :to="`/commit/${hash}`" v-test="'commit-link'">#{{hash.slice(0, 7)}}</nuxt-link><!--
-      --><a :href="`https://github.com/MakeNowJust/commlog/commit/${hash}`" v-test="'commit-external-link'"><icon name="external-link-alt" /></a>
+    <section :class="$style.foot">
+      <a v-if="author.github" :class="$style.author" :href="`https://github.com/${author.name}`">
+        {{author.name}}<img :src="author.icon" />
+      </a>
+      <span v-else :class="$style.author">{{author.name}}</span>
+      <time :datetime="date.toISOString()">{{date.toLocaleString()}}</time>
+      <nuxt-link :class="$style.hash" :to="`/commit/${hash}`" v-test="'commit-link'">#{{hash.slice(0, 7)}}</nuxt-link>
+      <a :href="`https://github.com/MakeNowJust/commlog/commit/${hash}`" v-test="'commit-external-link'">
+        <icon name="external-link-alt" />
+      </a>
     </section>
   </article>
 </template>
@@ -21,11 +23,16 @@
 }
 
 .foot {
-  font-size: 0.8rem;
+  font-size: 0;
   margin: 0 auto 1rem auto;
   padding: 0 0.5rem;
   max-width: $max-width;
   text-align: right;
+
+  > * {
+    font-size: 0.8rem;
+    display: inline-block;
+  }
 
   a {
     text-decoration: underline;

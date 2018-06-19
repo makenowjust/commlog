852df6beb38f1030c9716fa91d54a71fda84917a
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-06-19 09:07:26 +0900

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,19 @@
 module.exports = {
-  plugins: ['import', 'unicorn'],
-  extends: ['plugin:vue/recommended', 'xo-space/esnext', 'plugin:unicorn/recommended', 'prettier'],
+  plugins: ['import', 'promise', 'unicorn', 'vue'],
+  extends: [
+    'plugin:import/recommended',
+    'plugin:promise/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:vue/recommended',
+    'xo-space/esnext',
+    'prettier',
+  ],
   rules: {
-    'unicorn/filename-case': 'off',
-    'import/default': 'error',
-    'import/export': 'error',
-    'import/extensions': [
-      'error',
-      {
-        js: 'never',
-        json: 'never',
-        jsx: 'never',
-      },
-    ],
-    'import/first': 'error',
-    'import/named': 'error',
-    'import/namespace': ['error', {allowComputed: true}],
-    'import/no-absolute-path': 'error',
-    'import/no-webpack-loader-syntax': 'error',
-    'import/no-self-import': 'error',
-    'import/no-useless-path-segments': 'error',
-    'import/newline-after-import': 'error',
-    'import/no-amd': 'error',
-    'import/no-duplicates': 'error',
-    'import/no-extraneous-dependencies': 'error',
-    'import/no-mutable-exports': 'error',
-    'import/no-named-as-default-member': 'error',
-    'import/no-named-as-default': 'error',
-    'import/no-unresolved': ['error', {commonjs: true}],
-    'import/order': 'error',
+    'import/order': ['error', {'newlines-between': 'always'}],
     'import/no-unassigned-import': [
       'error',
       {
-        allow: ['babel-polyfill', '@babel/polyfill', 'babel-register', '@babel/register'],
+        allow: ['vue-awesome/icons/*'],
       },
     ],
     'vue/max-attributes-per-line': [
@@ -58,4 +39,12 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      files: 'components/*.vue',
+      rules: {
+        'unicorn/filename-case': ['error', {case: 'pascalCase'}],
+      },
+    },
+  ],
 };

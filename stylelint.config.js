c2f654c9a2e953cde27e1ffc645597b25a2e1893
TSUYUSATO Kitsune <make.just.on@gmail.com>
2018-08-25 11:12:03 +0900

    fix: introduce stylelint-config-css-modules

--- stylelint.config.js
+++ stylelint.config.js
@@ -6,12 +6,11 @@ module.exports = {
     'stylelint-config-xo-scss',
     'stylelint-config-xo-space',
     'stylelint-config-prettier',
+    'stylelint-config-css-modules',
   ],
   rules: {
     // 'order/properties-order' is disabled in 'stylelint-config-xo' for now.
     // See https://github.com/sindresorhus/stylelint-config-xo/pull/2#issuecomment-363438756.
     'order/properties-order': propertiesOrder,
-    // 'global' is known psedo class on CSS module.
-    'selector-pseudo-class-no-unknown': [true, {ignorePseudoClasses: ['global']}],
   },
 };

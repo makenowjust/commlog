60f5f0c1f603e71f442f838049c7dcf6dcf7544a
TSUYUSATO Kitsune <make.just.on@gmail.com>
2023-09-17 12:10:09 +0900

    Update build script

--- build.sh
+++ build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 # Install Bun
 curl -fsSL https://bun.sh/install | bash
 export BUN_INSTALL=$HOME/.bun
 export PATH=$BUN_INSTALL/bin:$PATH
-
-echo "bun --version"
 bun --version
 
 # Install deps
 bun install --frozen-lockfile
 
-echo "Build"
+# Build
 bun x nuxi generate

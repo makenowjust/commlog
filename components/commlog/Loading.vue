450e1b0ed6eb575f2ed2f954940adbad00f1f90c
TSUYUSATO Kitsune <make.just.on@gmail.com>
2023-10-05 16:44:45 +0900

    Fix Loading style

--- components/commlog/Loading.vue
+++ components/commlog/Loading.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="mx-auto container max-w-3xl text-center">
-    <div class="text-xl">Loading...</div>
-    <span class="loading loading-bars w-24" />
+  <div class="mx-auto container max-w-3xl h-48 text-center flex justify-center items-center">
+    <div>
+      <div class="text-xl">Loading...</div>
+      <span class="loading loading-bars w-24" />
+    </div>
   </div>
 </template>
--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -7,6 +7,8 @@ import COMMIT_536bd03 from "./data/commit-536bd0322ebc5f218208d52830efb5ba711021
 import COMMENTS_048ebce from "./data/comments-048ebceb14ff5367ad8ff9a8a64f920b5a3f9c6d.json";
 import SEARCH_HELLO from "./data/search-hello.json";
 
+const DELAY = 1000;
+
 const handleCommits = (
   req: RestRequest,
   res: ResponseFunction,
@@ -16,14 +18,14 @@ const handleCommits = (
   switch (page) {
     case "1":
       return res(
-        ctx.delay(1000),
+        ctx.delay(DELAY),
         ctx.status(200),
         ctx.set(COMMITS_1.headers),
         ctx.json(COMMITS_1.data),
       );
     case "2":
       return res(
-        ctx.delay(1000),
+        ctx.delay(DELAY),
         ctx.status(200),
         ctx.set(COMMITS_2.headers as any),
         ctx.json(COMMITS_2.data),
@@ -41,9 +43,9 @@ const handleCommit = (
   const hash = String(req.params.hash);
   switch (hash) {
     case "048ebceb14ff5367ad8ff9a8a64f920b5a3f9c6d":
-      return res(ctx.delay(1000), ctx.status(200), ctx.json(COMMIT_048ebce));
+      return res(ctx.delay(DELAY), ctx.status(200), ctx.json(COMMIT_048ebce));
     case "536bd0322ebc5f218208d52830efb5ba7110213e":
-      return res(ctx.delay(1000), ctx.status(200), ctx.json(COMMIT_536bd03));
+      return res(ctx.delay(DELAY), ctx.status(200), ctx.json(COMMIT_536bd03));
     default:
       return res(ctx.status(404));
   }
@@ -58,7 +60,7 @@ const handleCommitComments = (
   switch (hash) {
     case "048ebceb14ff5367ad8ff9a8a64f920b5a3f9c6d":
       return res(
-        ctx.delay(1500),
+        ctx.delay(DELAY * 1.5),
         ctx.status(200),
         ctx.set(COMMENTS_048ebce.headers as any),
         ctx.json(COMMENTS_048ebce.data),
@@ -77,7 +79,7 @@ const handleSearch = (
   switch (q) {
     case "repo:makenowjust/commlog hello":
       return res(
-        ctx.delay(1000),
+        ctx.delay(DELAY * 1.5),
         ctx.status(200),
         ctx.set(SEARCH_HELLO.headers as any),
         ctx.json(SEARCH_HELLO.data),
